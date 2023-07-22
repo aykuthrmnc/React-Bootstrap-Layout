abd2f84fa94759edd57d99a225cdc4eb830d05bf
aykut <aykut_h42@hotmail.com>
2023-07-22 14:39:00 +0300

    Sidebar düzenlendi.

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -1,156 +1,132 @@
 import classNames from "classnames";
-import { Container, Dropdown, Nav, Navbar, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { useState } from "react";
+import { Collapse, Dropdown, Nav, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { FaBuilding, FaCalendar, FaCalendarCheck, FaHome, FaPager, FaUser } from "react-icons/fa";
 import { useSelector } from "react-redux";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { RootState } from "~/store";
 import { setThemeHandle, userLogoutHandle } from "~/utils";
 
-const Sidebar = ({ change }: any) => {
+const Sidebar = () => {
+  const [isExpand, setIsExpand] = useState(true);
   const theme = useSelector((state: RootState) => state.auth.theme);
+  const [open, setOpen] = useState(false);
 
   return (
-    <div id="sidebar">
-      <Navbar
-        expand="lg"
-        className="bg-body-tertiary p-lg-0 sticky-lg-top w-xs-100"
-        style={{ width: change ? "4rem" : "18rem", transition: "width .2s ease" }}
+    <div id="sidebar" className="d-none d-lg-block">
+      <div
+        className="d-flex flex-column flex-shrink-0 bg-body-tertiary sticky-top min-vh-100"
+        style={{ width: isExpand ? "18rem" : "4rem", transition: "width .2s ease" }}
       >
-        <Container fluid className="px-lg-0 align-items-stretch min-vh-lg-100 gap-3">
-          <Navbar.Brand as={NavLink} to="/" className="d-flex d-lg-none align-items-center gap-2">
-            <FaHome size="24" />
-            <span>AYKUTHRMNC</span>
-          </Navbar.Brand>
-          <Navbar.Toggle className="border-0 shadow-none" />
-
-          <Navbar.Collapse className="flex-lg-column align-items-lg-stretch">
-            <Nav variant="pills" fill={change} className="flex-column mb-auto">
-              <OverlayTrigger
-                placement="right"
-                overlay={<Tooltip className={classNames("d-none", { "d-lg-block": change, "d-lg-none": !change })}>AYKUTHRMNC</Tooltip>}
+        <Nav variant="pills" fill={!isExpand} className="flex-column mb-auto">
+          {/* <OverlayTrigger placement="right" overlay={<Tooltip className={classNames({ "d-none": !change })}>ANASAYFA</Tooltip>}> */}
+          <Nav.Item>
+            <div className="position-relative">
+              <Nav.Link
+                as={NavLink}
+                to="/"
+                className={classNames("link-body-emphasis d-flex align-items-center gap-2 p-3 border-bottom rounded-0", {
+                  "mb-3 bg-transparent": isExpand,
+                  "justify-content-center": !isExpand,
+                })}
               >
-                <Nav.Item>
-                  <Nav.Link
-                    as={NavLink}
-                    to="/"
-                    className={classNames("link-body-emphasis d-none d-lg-flex align-items-center gap-2 p-3 border-bottom rounded-0 mx-lg-0", {
-                      "mb-lg-3 bg-transparent": !change,
-                      "border-bottom justify-content-center": change,
-                    })}
-                  >
-                    <FaHome size="24" />
-                    <span className={classNames({ "d-lg-none": change })}>AYKUTHRMNC</span>
-                  </Nav.Link>
-                </Nav.Item>
-              </OverlayTrigger>
-              <div className={classNames({ "px-lg-3": !change })}>
-                <OverlayTrigger
-                  placement="right"
-                  overlay={<Tooltip className={classNames("d-none", { "d-lg-block": change, "d-lg-none": !change })}>Personeller</Tooltip>}
+                <FaHome size="24" />
+                {isExpand && <span className="fs-5">AYKUTHRMNC</span>}
+              </Nav.Link>
+            </div>
+          </Nav.Item>
+          {/* </OverlayTrigger> */}
+          <div className={classNames({ "px-3": isExpand })}>
+            <OverlayTrigger placement="right" overlay={<Tooltip className={classNames({ "d-none": isExpand })}>Personeller</Tooltip>}>
+              <Nav.Item>
+                <Nav.Link as={NavLink} to="/personeller" className={classNames("link-body-emphasis", { "rounded-0 border-bottom": !isExpand })}>
+                  {isExpand ? "Personeller" : <FaUser />}
+                </Nav.Link>
+              </Nav.Item>
+            </OverlayTrigger>
+            <OverlayTrigger placement="right" overlay={<Tooltip className={classNames({ "d-none": isExpand })}>İzinler</Tooltip>}>
+              <Nav.Item>
+                <Nav.Link as={NavLink} to="/izinler" className={classNames("link-body-emphasis", { "rounded-0 border-bottom": !isExpand })}>
+                  {isExpand ? "İzinler" : <FaCalendarCheck />}
+                </Nav.Link>
+              </Nav.Item>
+            </OverlayTrigger>
+            <OverlayTrigger placement="right" overlay={<Tooltip className={classNames({ "d-none": isExpand })}>Takvim</Tooltip>}>
+              <Nav.Item>
+                <Nav.Link as={NavLink} to="/takvim" className={classNames("link-body-emphasis", { "rounded-0 border-bottom": !isExpand })}>
+                  {isExpand ? "Takvim" : <FaCalendar />}
+                </Nav.Link>
+              </Nav.Item>
+            </OverlayTrigger>
+            <OverlayTrigger placement="right" overlay={<Tooltip className={classNames({ "d-none": isExpand })}>Şirket</Tooltip>}>
+              <Nav.Item>
+                <Nav.Link as={NavLink} to="/sirket" className={classNames("link-body-emphasis", { "rounded-0 border-bottom": !isExpand })}>
+                  {isExpand ? "Şirket" : <FaBuilding />}
+                </Nav.Link>
+              </Nav.Item>
+            </OverlayTrigger>
+            <OverlayTrigger placement="right" overlay={<Tooltip className={classNames({ "d-none": isExpand })}>Raporlar</Tooltip>}>
+              {/* <Nav.Item>
+                <Nav.Link as={NavLink} to="/raporlar" className={classNames("link-body-emphasis", { "rounded-0 border-bottom": !isExpand })}>
+                  {isExpand ? "Raporlar" : <FaPager />}
+                </Nav.Link>
+              </Nav.Item> */}
+              <Nav.Item>
+                <Nav.Link
+                  onClick={() => setOpen(!open)}
+                  aria-expanded={open}
+                  className={classNames("link-body-emphasis", { "toggle d-flex align-items-center": isExpand, "rounded-0 border-bottom": !isExpand })}
                 >
-                  <div className="nav-item">
-                    <Nav.Link
-                      as={NavLink}
-                      to="/personeller"
-                      className={classNames("link-body-emphasis d-flex align-items-center justify-content-lg-center gap-2 text-start", {
-                        "text-lg-center rounded-0 border-bottom py-3": change,
-                      })}
-                    >
-                      <FaUser className={classNames({ "d-lg-none": !change })} />
-                      <span className={classNames("me-auto", { "d-lg-none": change })}>Personeller</span>
+                  {isExpand ? "Raporlar" : <FaPager />}
+                </Nav.Link>
+              </Nav.Item>
+            </OverlayTrigger>
+            <Collapse in={open}>
+              <div>
+                <OverlayTrigger placement="right" overlay={<Tooltip className={classNames({ "d-none": isExpand })}>Raporlar</Tooltip>}>
+                  <Nav.Item>
+                    <Nav.Link as={NavLink} to="/raporlar" className={classNames("link-body-emphasis", { "rounded-0 border-bottom": !isExpand })}>
+                      {isExpand ? "Raporlar 1" : <FaPager />}
                     </Nav.Link>
-                  </div>
+                  </Nav.Item>
                 </OverlayTrigger>
-                <OverlayTrigger
-                  placement="right"
-                  overlay={<Tooltip className={classNames("d-none", { "d-lg-block": change, "d-lg-none": !change })}>İzinler</Tooltip>}
-                >
-                  <div className="nav-item">
-                    <Nav.Link
-                      as={NavLink}
-                      to="/izinler"
-                      className={classNames("link-body-emphasis d-flex align-items-center justify-content-lg-center gap-2 text-start", {
-                        "text-lg-center rounded-0 border-bottom py-3": change,
-                      })}
-                    >
-                      <FaCalendarCheck className={classNames({ "d-lg-none": !change })} />
-                      <span className={classNames("me-auto", { "d-lg-none": change })}>İzinler</span>
+                <OverlayTrigger placement="right" overlay={<Tooltip className={classNames({ "d-none": isExpand })}>Raporlar</Tooltip>}>
+                  <Nav.Item>
+                    <Nav.Link as={NavLink} to="/raporlar2" className={classNames("link-body-emphasis", { "rounded-0 border-bottom": !isExpand })}>
+                      {isExpand ? "Raporlar 2" : <FaPager />}
                     </Nav.Link>
-                  </div>
-                </OverlayTrigger>
-                <OverlayTrigger
-                  placement="right"
-                  overlay={<Tooltip className={classNames("d-none", { "d-lg-block": change, "d-lg-none": !change })}>Takvim</Tooltip>}
-                >
-                  <div className="nav-item">
-                    <Nav.Link
-                      as={NavLink}
-                      to="/takvim"
-                      className={classNames("link-body-emphasis d-flex align-items-center justify-content-lg-center gap-2 text-start", {
-                        "text-lg-center rounded-0 border-bottom py-3": change,
-                      })}
-                    >
-                      <FaCalendar className={classNames({ "d-lg-none": !change })} />
-                      <span className={classNames("me-auto", { "d-lg-none": change })}>Takvim</span>
-                    </Nav.Link>
-                  </div>
-                </OverlayTrigger>
-                <OverlayTrigger
-                  placement="right"
-                  overlay={<Tooltip className={classNames("d-none", { "d-lg-block": change, "d-lg-none": !change })}>Şirket</Tooltip>}
-                >
-                  <div className="nav-item">
-                    <Nav.Link
-                      as={NavLink}
-                      to="/sirket"
-                      className={classNames("link-body-emphasis d-flex align-items-center justify-content-lg-center gap-2 text-start", {
-                        "text-lg-center rounded-0 border-bottom py-3": change,
-                      })}
-                    >
-                      <FaBuilding className={classNames({ "d-lg-none": !change })} />
-                      <span className={classNames("me-auto", { "d-lg-none": change })}>Şirket</span>
-                    </Nav.Link>
-                  </div>
-                </OverlayTrigger>
-                <OverlayTrigger
-                  placement="right"
-                  overlay={<Tooltip className={classNames("d-none", { "d-lg-block": change, "d-lg-none": !change })}>Raporlar</Tooltip>}
-                >
-                  <div className="nav-item">
-                    <Nav.Link
-                      as={NavLink}
-                      to="/raporlar"
-                      className={classNames("link-body-emphasis d-flex align-items-center justify-content-lg-center gap-2 text-start", {
-                        "text-lg-center rounded-0 border-bottom py-3": change,
-                      })}
-                    >
-                      <FaPager className={classNames({ "d-lg-none": !change })} />
-                      <span className={classNames("me-auto", { "d-lg-none": change })}>Raporlar</span>
-                    </Nav.Link>
-                  </div>
+                  </Nav.Item>
                 </OverlayTrigger>
               </div>
-            </Nav>
-            <Dropdown drop="end">
-              <Dropdown.Toggle
-                variant=""
-                bsPrefix={change ? "justify-content-center" : "gap-2"}
-                className="link-body-emphasis border-0 d-flex align-items-center w-100 border-top px-0 px-lg-3 py-2 rounded-0"
-              >
-                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
-                <span className={classNames({ "d-lg-none": change })}>Aykut Harmancı</span>
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item onClick={() => setThemeHandle(theme === "dark" ? "light" : "dark")}>Tema Değiştir</Dropdown.Item>
-                <Dropdown.Item>Ayarlar</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item onClick={() => userLogoutHandle()}>Çıkış Yap</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+            </Collapse>
+          </div>
+        </Nav>
+        <Dropdown drop="end">
+          <Dropdown.Toggle
+            variant=""
+            bsPrefix={isExpand ? "gap-2" : "justify-content-center"}
+            className="link-body-emphasis border-0 d-flex align-items-center w-100 border-top py-2 rounded-0"
+          >
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
+            {isExpand && <span>Aykut Harmancı</span>}
+          </Dropdown.Toggle>
+          <Dropdown.Menu align="end" className="p-2">
+            <Dropdown.Item className="rounded-1" onClick={() => setIsExpand(!isExpand)}>
+              {isExpand ? "Menüyü Daralt" : "Menüyü Genişlet"}
+            </Dropdown.Item>
+            <Dropdown.Item className="rounded-1" onClick={() => setThemeHandle(theme === "dark" ? "light" : "dark")}>
+              Tema Değiştir
+            </Dropdown.Item>
+            <Dropdown.Item className="rounded-1">Ayarlar</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item className="rounded-1" onClick={() => userLogoutHandle()}>
+              Çıkış Yap
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
     </div>
   );
 };
+
 export default Sidebar;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,18 +1,14 @@
 import { Outlet } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Container, Fade } from "react-bootstrap";
 import Sidebar from "~/components/Layout/Sidebar";
-import { useState } from "react";
 
 const MainLayout = () => {
-  const [change, setChange] = useState(false);
-
   return (
     <div className="d-flex flex-column flex-lg-row">
-      <Sidebar change={change} />
+      <Sidebar />
       {/* <Header2 /> */}
       {/* <Header /> */}
       <Container fluid className="p-3">
-        <button onClick={() => setChange(!change)}>Değiştir</button>
         <Outlet />
       </Container>
     </div>
